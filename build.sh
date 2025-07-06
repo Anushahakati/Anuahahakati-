51687ac08c63cd2bda9436eb25672ea94b68ed55
Anusha Hakati <anushahakati15s1861@gmail.com>
2025-07-06 10:35:32 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    libgtk-3-dev \
+    libboost-all-dev \
+    python3-dev
+
+pip install --upgrade pip
+pip install -r requirements.txt
